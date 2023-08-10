2d2477b0ca6c357c9a9c04ced8fe119826df76f0
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-08-10 00:07:18 -0300

    protegendo contra serializacao e endpoint findbyid para tecnico

---
+++ src/main/java/com/marcelino/helpdesk/services/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.marcelino.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcelino.helpdesk.domain.Tecnico;
+import com.marcelino.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+
+	@Autowired
+	private TecnicoRepository repository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
