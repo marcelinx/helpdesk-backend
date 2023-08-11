942fa5a5a0c11bb64d08e687d4cde6154da7c4fb
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-08-11 13:57:05 -0300

    validacao nos campos de criacao de novo usuario

--- src/main/java/com/marcelino/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/marcelino/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -2,6 +2,8 @@ package com.marcelino.helpdesk.resources.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -30,5 +32,18 @@ public class ResourceExceptionHandler {
 				"Violacao de dados", ex.getMessage(), request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex, 
+			HttpServletRequest request) {
+		
+		ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), 
+				"Validation error", "erro na validacao dos campos", request.getRequestURI());
+		
+		for(FieldError x : ex.getBindingResult().getFieldErrors()) {
+			errors.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 }
