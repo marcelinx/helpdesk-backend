2a02811f0c2f4ccea1a4e2171f0db0c0d58623db
Joao Luiz <joaoluizmarcelinoribeiro@gmail.com>
2023-08-11 17:21:14 -0300

    delete para tecnico implementado

--- src/main/java/com/marcelino/helpdesk/services/TecnicoService.java
+++ src/main/java/com/marcelino/helpdesk/services/TecnicoService.java
@@ -48,6 +48,14 @@ public class TecnicoService {
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);
 	}
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		if(obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Tecnico possui ordens de servicos e nao pode ser deletado!");
+		} else {
+			repository.deleteById(id);;
+		}
+	}
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -60,5 +68,6 @@ public class TecnicoService {
 			throw new DataIntegrityViolationException("E-mail ja cadastrado no sistema!");
 		}
 	}
+
 	
 }
